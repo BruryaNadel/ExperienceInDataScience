b017d193ec27c55748e509cb0f9e727e731864b9
Brurya <76554841+BruryaNadel@users.noreply.github.com>
2023-05-11 09:40:28 +0300

    add my first notebook file

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# ## In this notebook, Data Science Tools and Ecosystem are summarized
+
+# **Objectives**
+# * SQL
+# * Python
+# * R
+# * JS
+# * Julya
+
+# ### Some of the popular languages that Data Scientists use are:
+# * Python
+# * R
+# * SQL
+
+# ### Some of the commonly used libraries used by Data Scientists include:
+# * Ggplot2
+# * Pandas
+# * Vegas
+
+# | Data Science Tools |
+# | :- |
+# | GoogleColaboratory |
+# | Anaconda |
+# | Visual Basic Studio |
+
+# ### Below are a few examples of evaluating arithentic in python
+
+# In[1]:
+
+
+# This a sample arithmetic to multiply then add integers
+5+(4*3)
+
+
+# In[2]:
+
+
+# This will convert 200 minutes to hours by diving by 60
+200/60
+
+
+# ## Author
+# bruria
