14f0c70b7e2f2c9ae91ef378435fa4f1c368dc82
Brurya <76554841+BruryaNadel@users.noreply.github.com>
2023-05-21 10:44:32 +0300

    updating the excerise files

---
+++ DataScienceEcosystem (1).py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# ## In this notebook, Data Science Tools and Ecosystem are summarized
+
+# **Objectives**
+# * Add comments to code cells
+# * Create markdown cells
+# * Add orderd and unorderd lists to a markdown cells
+# * Format content of markdown cells with heading styles
+
+# ### Some of the popular languages that Data Scientists use are:
+# 1. Python
+# 2. R
+# 3.SQL
+
+# ### Some of the commonly used libraries used by Data Scientists include:
+# 1. Ggplot2
+# 2. Pandas
+# 3. Vegas
+
+# | Data Science Tools |
+# | :- |
+# | GoogleColaboratory |
+# | Anaconda |
+# | Visual Basic Studio |
+
+# ### Below are a few examples of evaluating arithentic in python
+
+# In[1]:
+
+
+# This a sample arithmetic to multiply then add integers
+5+(4*3)
+
+
+# In[2]:
+
+
+# This will convert 200 minutes to hours by diving by 60
+round(200/60, 2)
+
+
+# ## Author
+# bruria
